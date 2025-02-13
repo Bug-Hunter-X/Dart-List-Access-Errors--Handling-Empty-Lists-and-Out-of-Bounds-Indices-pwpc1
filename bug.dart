0b959660f163fbc3bc03c19d7f35b980652563e8
BugHunter-X <askzenai@gmail.com>
2025-02-13 04:56:05 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,12 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+
+//This will throw an error if the list is empty
+int firstNumber = numbers.first;
+
+//This will throw an error if the list is empty
+int lastNumber = numbers.last;
+
+//This will throw an error if the index is out of range
+int numberAtIndex = numbers[10];
+```
